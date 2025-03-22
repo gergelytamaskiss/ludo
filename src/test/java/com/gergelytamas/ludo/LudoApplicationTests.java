2f80f562a84b7777e8abe70a9d13a42218bdf95e
Kiss Gergely <gergelytamas.kiss@gmail.com>
2025-03-22 14:18:17 +0100

    feat: Initial commit

---
+++ src/test/java/com/gergelytamas/ludo/LudoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gergelytamas.ludo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//
+	}
+
+}
